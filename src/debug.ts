3ba55b17d2f9865c9fd7216e51f945543d12d998
Cameron Tacklind <cameron@tacklind.com>
2019-02-17 16:51:51 -0800

    Debug function

---
+++ src/debug.ts
@@ -0,0 +1,106 @@
+'use strict';
+
+import USB, {
+  addAttachListener,
+  CommandMode,
+  MLXCommand,
+  Command,
+  ReadData,
+} from './USBInterface';
+
+import chalk from 'chalk';
+import { makeMLXPacket, Opcode, Marker } from './MLX90363';
+
+const serial = 'None';
+const usb = USB(serial);
+
+function delay(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const GetAlpha: MLXCommand = {
+  mode: CommandMode.MLXDebug,
+  data: makeMLXPacket({
+    opcode: Opcode.GET1,
+    marker: Marker.Alpha,
+    data16: [, 0xffff],
+  }),
+};
+const GetXYZ: MLXCommand = {
+  mode: CommandMode.MLXDebug,
+  data: makeMLXPacket({
+    opcode: Opcode.GET1,
+    marker: Marker.XYZ,
+    data16: [, 0xffff],
+  }),
+};
+const MLXNOP: MLXCommand = {
+  mode: CommandMode.MLXDebug,
+  data: makeMLXPacket({ opcode: Opcode.NOP__Challenge }),
+};
+
+function sendCommand(command: Command) {
+  console.log(command);
+  return new Promise(res => {
+    try {
+      usb.write(command, res);
+    } catch (e) {
+      console.log('not Sent because:', e);
+      res();
+    }
+  });
+}
+
+const mode = CommandMode.Calibration;
+
+// Ramp amplitude up slowly
+let amplitude = 0;
+
+usb.events.on('status', async (s: string) => {
+  if (s != 'ok') return;
+
+  // Motor connected
+
+  console.log('Starting');
+
+  await sendCommand(GetAlpha);
+  // Give sensor time to make reading
+  await delay(10);
+  await sendCommand(GetXYZ);
+
+  const xyzDelay = delay(10);
+
+  // Double read to force reading of newest data
+  await usb.read();
+  let data: false | ReadData;
+
+  await delay(1);
+
+  while (true) {
+    data = await usb.read();
+    if (!data) throw 'Data missing?';
+    if (!data.mlxParsedResponse) {
+      console.log('No parsed response');
+      await delay(100);
+      return;
+      continue;
+    } else break;
+  }
+
+  if (data.mlxParsedResponse.opcode == Opcode.Error_frame) {
+    console.log('Error frame. Error:', data.mlxParsedResponse.error);
+    await delay(10);
+    throw 'Received Error Frame';
+  }
+
+  if (data.mlxParsedResponse.opcode == Opcode.NothingToTransmit) {
+    console.log('NTT');
+    await delay(10);
+    throw 'Nothing to transmit';
+  }
+
+  const { alpha } = data.mlxParsedResponse;
+});
+
+// Actually start looking for the usb device
+usb.start(false);
