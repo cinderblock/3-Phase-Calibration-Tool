1c6a5f9c61ed8f06925ec872d2ea567da56491d8
Cameron Tacklind <cameron@tacklind.com>
2019-03-18 14:33:38 -0700

    Close cleanly. Report writes per second. Write faster.

--- src/Oscillate.ts
+++ src/Oscillate.ts
@@ -43,57 +43,137 @@ async function main() {
   stopAttachListening();
 
   const usb = USBInterface(serial);
-
-  usb.events.on('data', (data: ReadData) => {
-    if (!calibrated && data.calibrated) {
-      calibrated = true;
-      console.log('Calibrated!');
-    }
-
-    if (data.state !== lastState) {
-      lastState = data.state;
-
-      if (data.state === ControllerState.Fault) {
-        lastFault = data.fault;
-        console.log(
-          'New State:',
-          ControllerState[data.state],
-          ' - ',
-          ControllerFault[data.fault],
-          data.fault
-        );
-      } else {
-        console.log('New State:', ControllerState[data.state]);
-      }
-    } else if (
-      data.state == ControllerState.Fault &&
-      data.fault !== lastFault
-    ) {
-      console.log('New Fault:', ControllerFault[data.fault]);
-    }
-
-    // console.log({ calibrated, ...data });
-  });
-
   const Frequency = 0.5;
 
-  usb.events.on('status', (s: string) => {
+  const dataListener = async (s: string) => {
     if (s != 'ok') return;
 
-    // Motor connected
+    usb.events.removeListener('status', dataListener);
 
-    console.log('Starting');
+    await new Promise<void>((resolve, reject) => {
+      usb.events.once('data', (data: ReadData) => {
+        lastState = data.state;
+        lastFault = data.fault;
+        resolve();
+      });
+    });
 
-    const zero = Date.now();
+    // Test initial state
+    if (lastState !== ControllerState.Fault) {
+      console.log('Unexpected initial state:', ControllerState[lastState]);
+      console.log('Continuing...');
+    } else if (lastFault !== ControllerFault.Init) {
+      console.log('Unexpected initial fault:', ControllerFault[lastFault]);
 
-    const i = setInterval(async () => {
-      const command =
-        amplitude *
-        Math.sin(((Date.now() - zero) / 1000) * 2 * Math.PI * Frequency);
+      await new Promise<void>((resolvePromise, reject) => {
+        const resolve = () => {
+          usb.events.removeListener('data', l);
+          resolvePromise();
+        };
 
-      usb.write({ mode, command });
-    }, 1000 / 60);
-  });
+        const l = (data: ReadData) => {
+          console.log(data.current);
+          lastState = data.state;
+          lastFault = data.fault;
+          if (
+            data.state === ControllerState.Fault &&
+            data.fault === ControllerFault.Init
+          )
+            resolve();
+        };
+
+        usb.events.on('data', l);
+
+        usb.write({ mode: CommandMode.ClearFault });
+
+        console.log('Clearing fault. Waiting for fault to be cleared.');
+      });
+    } else {
+      // Good to go!
+      console.log('Starting normally!');
+    }
+
+    // await new Promise<void>((resolve, reject) => {
+    //   usb.events.on('data', (data: ReadData) => {
+    //     lastState = data.state;
+    //     lastFault = data.fault;
+    //     resolve();
+    //   });
+    // });
+
+    usb.events.once('data', (data: ReadData) => {
+      if (!data.calibrated) {
+        console.log('Uncalibrated!');
+        usb.close();
+        return;
+      }
+      console.log('Calibrated!');
+
+      usb.events.on('data', (data: ReadData) => {
+        if (data.state !== lastState) {
+          lastState = data.state;
+
+          if (data.state === ControllerState.Fault) {
+            lastFault = data.fault;
+            console.log(
+              'New State:',
+              ControllerState[data.state],
+              ' - ',
+              ControllerFault[data.fault],
+              data.fault
+            );
+          } else {
+            console.log('New State:', ControllerState[data.state]);
+          }
+        } else if (
+          data.state == ControllerState.Fault &&
+          data.fault !== lastFault
+        ) {
+          console.log('New Fault:', ControllerFault[data.fault]);
+        }
+
+        // console.log({ calibrated, ...data });
+      });
+
+      // Motor connected
+
+      console.log('Starting');
+
+      let writes = 0;
+
+      const WPS = setInterval(() => {
+        console.log('WPS:', writes);
+        writes = 0;
+      }, 1000);
+
+      const zero = Date.now();
+
+      const i = setInterval(async () => {
+        const command =
+          amplitude *
+          Math.sin(((Date.now() - zero) / 1000) * 2 * Math.PI * Frequency);
+
+        usb.write({ mode, command });
+        writes++;
+      }, 1000 / 300);
+
+      function die() {
+        // Shutdown running write loop
+        clearInterval(i);
+        // Stop the motor
+        usb.write({ mode, command: 0 }, usb.close);
+        // Close USB connection
+        usb.close();
+
+        // Just in case, really exit after a short delay.
+        setTimeout(process.exit, 400).unref();
+      }
+
+      process.on('SIGINT', die);
+    });
+  };
+
+  usb.events.on('status', dataListener);
 
   // Actually start looking for the usb device
   usb.start();
