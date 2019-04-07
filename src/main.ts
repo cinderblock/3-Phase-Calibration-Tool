14f545aa4a9e9f70185ce6e4e01dfd48d48e6829
Cameron Tacklind <cameron@tacklind.com>
2019-04-07 12:18:03 -0700

    Add new Raw XYZ printer

--- src/main.ts
+++ src/main.ts
@@ -538,6 +538,105 @@ async function writeRawDataToPNG(filename: string, processed: ProcessedData, wid
   chartNode.writeImageToFile('image/png', filename);
 }
 
+async function writeRawXYZToPNG(filename: string, dataPoints: DataPoint[], width = 600, height = width) {
+  const chartNode = new ChartjsNode(width, height);
+  await chartNode.drawChart({
+    type: 'scatter',
+    data: {
+      datasets: [
+        {
+          label: 'X',
+          yAxisID: 'XYZ',
+          data: dataPoints.map(({ x }, s) => ({ x: s, y: x })),
+          backgroundColor: '#ff0000',
+        },
+        {
+          label: 'Y',
+          yAxisID: 'XYZ',
+          data: dataPoints.map(({ y }, s) => ({ x: s, y: y })),
+          backgroundColor: '#00ff00',
+        },
+        {
+          label: 'Z',
+          yAxisID: 'XYZ',
+          data: dataPoints.map(({ z }, s) => ({ x: s, y: z })),
+          backgroundColor: '#0000ff',
+        },
+        {
+          label: 'Gain',
+          yAxisID: 'VG',
+          data: dataPoints.map(({ VG }, s) => ({ x: s, y: VG })),
+          backgroundColor: '#000000',
+        },
+        {
+          label: 'Alpha',
+          yAxisID: 'Alpha',
+          data: dataPoints.map(({ alpha }, s) => ({ x: s, y: alpha })),
+          backgroundColor: '#e541f4',
+        },
+        {
+          label: 'Angle',
+          yAxisID: 'Alpha',
+          data: dataPoints.map(({ x, y }, s) => ({
+            x: s,
+            y: (Math.atan2(-y, -x) / (Math.PI * 2) + 0.5) * 2 ** 14,
+          })),
+          backgroundColor: '#fff45b',
+        },
+      ],
+    },
+    options: {
+      legend: { labels: { fontSize: 24 } },
+      scales: {
+        xAxes: [
+          {
+            scaleLabel: {
+              fontSize: 24,
+              display: true,
+              labelString: 'Drive Angle',
+            },
+            type: 'linear',
+            ticks: {
+              stepSize: cycle,
+              major: {
+                stepSize: cyclesPerRev,
+              },
+            },
+          },
+        ],
+        yAxes: [
+          {
+            id: 'XYZ',
+            scaleLabel: { fontSize: 24, display: true, labelString: 'Raw XYZ' },
+            type: 'linear',
+            position: 'left',
+            ticks: {
+              beginAtZero: true,
+            },
+          },
+          {
+            id: 'VG',
+            scaleLabel: { fontSize: 24, display: true, labelString: 'Gain' },
+            type: 'linear',
+            position: 'right',
+            ticks: {
+              beginAtZero: true,
+            },
+          },
+          {
+            id: 'Alpha',
+            type: 'linear',
+            position: 'right',
+            display: false,
+          },
+        ],
+      },
+    },
+  });
+
+  chartNode.writeImageToFile('image/png', filename);
+}
+
 async function writeScaledXYZToPNG(filename: string, dataPoints: DataPoint[], width = 600, height = width) {
   const chartNode = new ChartjsNode(width, height);
   await chartNode.drawChart({
