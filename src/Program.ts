5969fd78806f6a2a4e01e0a582b26dd6721c45cb
Cameron Tacklind <cameron@tacklind.com>
2019-04-08 15:04:02 -0700

    Add initial version of code to jump to programming mode

---
+++ src/Program.ts
@@ -0,0 +1,58 @@
+'use strict';
+
+import USB, { addAttachListener, CommandMode, Command } from 'smooth-control';
+import readline from 'readline';
+import chalk from 'chalk';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function delay(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function prompt(prompt: string) {
+  return new Promise<string>((resolve, reject) => {
+    rl.question(prompt, resolve);
+  });
+}
+
+async function main() {
+  rl.on('SIGINT', () => {
+    setTimeout(() => {
+      console.log('Forcing quit');
+      process.exit(0);
+    }, 400).unref();
+  });
+
+  let def = 'N/A';
+
+  const stopListening = await addAttachListener(id => {
+    console.log('\r', chalk.grey(new Date().toLocaleTimeString()), id);
+    def = id;
+    console.log(`Serial Number [${def}]: `);
+  });
+
+  // TODO: wait until first connected device is detected
+
+  const serial = (await prompt(`Serial Number [${def}]: `)).trim() || def;
+
+  stopListening();
+
+  rl.close();
+
+  const usb = USB(serial);
+
+  await new Promise<void>(resolve => usb.write(({ mode: CommandMode.Bootloader } as unknown) as Command, resolve));
+
+  usb.close();
+
+  setTimeout(() => {
+    console.log('Force killing');
+    process.kill(0);
+  }, 500).unref();
+}
+
+main();
