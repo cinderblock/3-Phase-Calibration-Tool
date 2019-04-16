94a15880c7c1f8fabb4ccd10c6c0a0bf802147b4
Cameron Tacklind <cameron@tacklind.com>
2019-04-16 14:47:38 -0700

    Use promises instead of Sync

--- src/outputs/CalibrationBlock.ts
+++ src/outputs/CalibrationBlock.ts
@@ -1,4 +1,4 @@
-import { writeFileSync } from 'fs';
+import { promises as fs } from 'fs';
 import MemoryMap from 'nrf-intel-hex';
 import { EOL } from 'os';
 
@@ -7,5 +7,5 @@ export default async function writeCalibrationBlock(filename: string, block: Buf
 
   mem.set(0x4f80, block);
 
-  writeFileSync(filename, mem.asHexString().replace(/\n/g, EOL) + EOL);
+  return fs.writeFile(filename, mem.asHexString().replace(/\n/g, EOL) + EOL);
 }
