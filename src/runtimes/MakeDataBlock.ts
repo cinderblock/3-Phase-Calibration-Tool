9a65efbe3df434e97ec97684b5d88e9261bad0ad
Cameron Tacklind <cameron@tacklind.com>
2019-04-21 20:58:00 -0700

    Initial version of script to import old csv style data and make a modern data block

--- src/runtimes/MakeDataBlock.ts
+++ src/runtimes/MakeDataBlock.ts
@@ -1,26 +1,72 @@
 'use strict';
 
-import USB, { addAttachListener, CommandMode, MLXCommand, Command } from 'smooth-control';
-
 import { v1 as uuid } from 'uuid';
-
-import readline from 'readline';
-import { createReadStream, createWriteStream, writeFileSync } from 'fs';
-import { EOL } from 'os';
 import DataIDBlock from '../processes/DataIDBlock';
-import chalk from 'chalk';
-import MemoryMap from 'nrf-intel-hex';
-import { makePacket, Opcode, Marker, ErrorCode } from 'mlx90363';
-import ChartjsNode from 'chartjs-node';
+import * as CLI from '../utils/CLI';
+import writeLookupTableToPNG from '../outputs/images/Lookup';
+import writeCalibrationBlock from '../outputs/CalibrationBlock';
 
-const chartWidth = 600;
-const chartHeight = chartWidth;
+const defaultRevolutions = 7;
 
-const cyclesPerRev = 15;
-const revolutions = 1;
+export default async function main() {
+  let serial = uuid();
+  serial = (await CLI.prompt('New Serial [' + serial + ']:')).trim() || serial;
 
-const cycle = 3 * 256;
+  console.log('Using serial:', serial);
 
-const maxAmplitude = 45;
+  const inputDate = (await CLI.prompt('Calibration Date [now]:')).trim();
 
-const rawDataFilename = 'data.csv';
+  const revolutions =
+    Number((await CLI.prompt('Revolutions [' + defaultRevolutions + ']:')).trim()) || defaultRevolutions;
+
+  CLI.close();
+
+  const calibrationTime = inputDate ? new Date(inputDate) : new Date();
+
+  const lookupTable = await readTable();
+
+  const dummy: number[] = [];
+
+  await Promise.all([
+    writeLookupTableToPNG(
+      'Lookup Table.png',
+      {
+        inverseTable: lookupTable,
+        forward: dummy,
+        middle: dummy,
+        reverse: dummy,
+        forwardData: dummy,
+        reverseData: dummy,
+      },
+      revolutions
+    ),
+    writeCalibrationBlock(serial + '.hex', DataIDBlock({ lookupTable, calibrationTime, serial })),
+  ]);
+
+  console.log('Done');
+}
+
+async function readTable(): Promise<number[]> {
+  return new Promise(async (resolve, reject) => {
+    const result: number[] = [];
+
+    while (result.length < 2 ** 12) {
+      (await CLI.prompt('Data:'))
+        .trim()
+        .split(',')
+        .map(s => s.trim())
+        .filter(s => s != '')
+        // This even handles base prefixed number strings
+        .map(Number)
+        .forEach(n => result.push(n));
+    }
+
+    if (result.length != 2 ** 12) {
+      reject('Incorrect number of elements read...');
+    } else {
+      resolve(result);
+    }
+  });
+}
+
+if (require.main === module) main();
