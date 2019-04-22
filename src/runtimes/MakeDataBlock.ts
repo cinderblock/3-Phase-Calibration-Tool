7ae9ed98d0c4d0c6efa5704e67d227ab930c6048
Cameron Tacklind <cameron@tacklind.com>
2019-04-21 22:32:15 -0700

    Load from a file instead

--- src/runtimes/MakeDataBlock.ts
+++ src/runtimes/MakeDataBlock.ts
@@ -5,11 +5,15 @@ import DataIDBlock from '../processes/DataIDBlock';
 import * as CLI from '../utils/CLI';
 import writeLookupTableToPNG from '../outputs/images/Lookup';
 import writeCalibrationBlock from '../outputs/CalibrationBlock';
+import { createReadStream } from 'fs';
+import readline from 'readline';
 
 // TODO: Load from smooth-control
 const cycle = 3 * 256;
 
-export default async function main() {
+export default async function main(file: string) {
+  console.log('Loading file:', file);
+
   const id = uuid();
   const serial = (await CLI.prompt('New Serial [' + id + ']:')).trim() || id;
 
@@ -21,7 +25,7 @@ export default async function main() {
 
   const calibrationTime = inputDate ? new Date(inputDate) : new Date();
 
-  const lookupTable = await readTable();
+  const lookupTable = await readTable(file);
 
   const revolutions = lookupTable.reduce((revs, pos) => Math.max(revs, Math.floor(pos / cycle)), 0) + 1;
 
@@ -48,26 +52,39 @@ export default async function main() {
   console.log('Done');
 }
 
-async function readTable(): Promise<number[]> {
+async function readTable(file: string): Promise<number[]> {
   return new Promise(async (resolve, reject) => {
+    const readStream = createReadStream(file);
+    const rl = readline.createInterface(readStream);
     const result: number[] = [];
     const expectedLength = 2 ** 12;
 
-    while (result.length < expectedLength) {
-      (await CLI.prompt('Data:'))
-        .trim()
-        .split(/[^0-9oxb]+/)
+    rl.on('line', l => {
+      l.split(/[^0-9oxb]+/)
+        // Filter empty strings and other bad values
         .filter(s => s)
         // This even handles base prefixed number strings
         .forEach(n => result.push(Number(n)));
-    }
+    });
 
-    if (result.length != expectedLength) {
-      reject('Incorrect number of elements read');
-    } else {
-      resolve(result);
-    }
+    rl.on('close', () => {
+      if (result.length != expectedLength) {
+        reject('Incorrect number of elements read');
+      } else {
+        console.log('done!');
+        resolve(result);
+      }
+
+      // Is this necessary?
+      readStream.close();
+    });
   });
 }
 
-if (require.main === module) main();
+if (require.main === module) {
+  const file = process.argv[2];
+  if (file) main(file);
+  else {
+    console.log('Must specify filename at command line');
+  }
+}
