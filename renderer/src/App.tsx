648fc337ae6873f77371513e1985215b46a4da7e
Cameron Tacklind <cameron@tacklind.com>
2020-01-11 20:44:03 -0800

    Show nice motor list

--- renderer/src/App.tsx
+++ renderer/src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import { BackendStateData } from './BackendConnection/BackendState';
+import { BackendStateData, useBackendStateUpdate } from './BackendConnection/BackendState';
 import { useUserControls } from './BackendConnection/UserControls';
 
+function MotorButton({ motor: serial }: { motor: string }) {
+  return <div>serial: {serial}</div>;
+}
+
 const App: React.FC = () => {
+  const connected = useBackendStateUpdate(s => s.connectedMotorSerials);
+
+  const motorList = !connected ? 'No Motors :(' : connected.map(serial => <MotorButton key={serial} motor={serial} />);
+
   return (
     <div className="App">
       <button onClick={useUserControls(() => ({}))}>Click me!</button>
       Time: <BackendStateData mapper={s => s.time} />
+      {motorList}
     </div>
   );
 };
