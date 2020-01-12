4d965784173ceb2161f76595c6501d9b5b3b34b6
Cameron Tacklind <cameron@tacklind.com>
2020-01-11 19:21:54 -0800

    Startup on secondary monitor in dev mode

--- main/src/electron.ts
+++ main/src/electron.ts
@@ -8,12 +8,34 @@ const { join } = posix;
 let mainWindow: BrowserWindow;
 
 function createWindow(): void {
+  let offset: { x: number; y: number };
+
+  if (isDev) {
+    const displays = screen.getAllDisplays();
+    const externalDisplay =
+      // For Cameron's dev machine
+      displays[2] ||
+      // For people that have 2 screens
+      displays.find(display => {
+        return display.bounds.x !== 0 || display.bounds.y !== 0;
+      });
+
+    if (externalDisplay) {
+      offset = {
+        x: externalDisplay.bounds.x + 150,
+        y: externalDisplay.bounds.y + 50,
+      };
+    }
+  }
+
   mainWindow = new BrowserWindow({
     width: 900,
     height: 680,
     webPreferences: {
       nodeIntegration: true,
     },
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    ...offset!,
   });
 
   if (isDev) {
