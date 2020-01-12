4475853e74b0b8643c34edc2933bd7fef76ccdc9
Cameron Tacklind <cameron@tacklind.com>
2020-01-11 19:18:36 -0800

    Vestigial

--- renderer/src/index.tsx
+++ renderer/src/index.tsx
@@ -3,10 +3,4 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-declare global {
-    interface Window {
-      require: any;
-    }
-}
-
 ReactDOM.render(<App />, document.getElementById('root'));
